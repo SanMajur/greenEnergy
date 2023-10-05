d120bbeb4bdb3e38e9dccb88bb977618d4498441
santino majur <demalong22@gmail.com>
2023-10-05 12:12:39 +0300

    dropdown quick links

---
+++ public/js/main.js
@@ -0,0 +1,21 @@
+const loginBtn = document.getElementById('loginBtn')
+const login = document.getElementById('loginOptions')
+const globalSitesBtn = document.getElementById('sitesBtn')
+const globalSites = document.getElementById('sitesDropdown')
+const dropdownBtn = document.querySelector('.dropdownBtn')
+
+ loginBtn.addEventListener('click', () => {
+     if (login.classList.contains('hidden')){
+         login.classList.remove('hidden')
+     } else{
+         login.classList.add('hidden')
+     }
+ })
+
+ globalSitesBtn.addEventListener('click', () => {
+    if (globalSites.classList.contains('hidden')){
+        globalSites.classList.remove('hidden')
+    } else{
+        globalSites.classList.add('hidden')
+    }
+})
